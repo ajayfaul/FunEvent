3a794afab1773363ca00fc50194a4cc9af35bfdb
ajayfaul <muhammadfajarm@gmail.com>
2019-07-08 16:11:43 +0700

    Modify SplashScreenActivity

--- app/src/main/java/com/morningstar/funevent/SplashScreenActivity.java
+++ app/src/main/java/com/morningstar/funevent/SplashScreenActivity.java
@@ -1,7 +1,10 @@
 package com.morningstar.funevent;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+
+            }
+        },3000);
     }
 }
